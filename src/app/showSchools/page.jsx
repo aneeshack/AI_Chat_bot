0c4280998b715173b0e7151f6d4843a672238f79
aneeshack <aneeshack08@gmail.com>
2025-09-05 00:48:06 +0530

    version1

--- src/app/showSchools/page.jsx
+++ src/app/showSchools/page.jsx
@@ -1,7 +1,7 @@
 import SchoolCard from '../../components/SchoolCard'
 
 async function getSchools() {
-    const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:3000'
+    const baseUrl = process.env.NEXT_PUBLIC_BASE_URL 
 
 
   // const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/schools`, {
